f0d182290a7e5cdd01823a0c179c06cd608ee6fd
Harshit katiyar <skkatiyar95065@gmail.com>
2024-12-29 22:58:47 +0530

    build components

---
+++ src/components/Head.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <div>
+        <div>
+           <img 
+           src='https://cdn-icons-png.flaticon.com/128/2791/2791469.png'
+           alt='menu icon'
+         />
+            <img
+            src='https://cdn-icons-png.flaticon.com/512/1384/1384060.png'
+            alt='yt icon'
+         />
+        </div>
+        <div>
+            <input type='text' /> 
+            <button>Search</button>
+        </div>
+        <div>
+            <img 
+            src='https://cdn-icons-png.flaticon.com/512/3177/3177440.png'
+            alt='user logo'
+          />  
+        </div>
+    </div>
+  )
+}
+
+export default Head;
